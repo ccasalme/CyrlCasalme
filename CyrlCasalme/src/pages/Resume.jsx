1b425f5007780566503fd94188d4db7fc24077e9
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-01-14 22:57:59 -0500

    added details to the resume

--- CyrlCasalme/src/pages/Resume.jsx
+++ CyrlCasalme/src/pages/Resume.jsx
@@ -7,22 +7,162 @@ export default function Resume() {
         <h1>Resume</h1>
         <CCard>
             <CCardBody>
-                This is some text within a card body.
+                <h3>Professional Competencies</h3>
             </CCardBody>
         </CCard>
         <br />
         <br />
         <CCard>
             <CCardBody>
-                This is some text within a card body.
+            <h4>Technical Proficiency:</h4>
+            </CCardBody>
+            <br />
+                <CCardBody>
+                  <h5>Web Development || Full Stack</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Front-End & Back-End:</h6>
+                  <p>HTML, CSS, WireFrames, JavaScript, TypeScript, React, SQL, Node.JS, Prompt Engineering, Browser APIs, Python Fundamentals, OOP with Python, DOM Manipulation, Terminal Commands, Object-oriented programming, MVC paradigm, MERN Stack, Testing, CI/CD Pipelines</p> 
+                  </CCardBody>
+                    <CCardBody>
+                    <h6>Version Control & Deployment:</h6>
+                    <p>GitHub Copilot, Team collaboration, Agile development, Project demonstration and Storytelling</p>
+                    </CCardBody>
+              <br />
+              <CCardBody>
+                <h5>Technical Support</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Technical Support:</h6>
+                <p>Support ticket management, Troubleshooting, Documentation, Process Improvement, Customer Retention, Relationship Management</p>
+                </CCardBody>
+              <br />
+              <CCardBody>
+              <h5>CRM, Digital Design, and Relevant Office Skills:</h5>
+              </CCardBody>
+                <CCardBody>
+                  <h6>Gooogle Suite:</h6>
+                  <p>Google Docs, Google Sheets, Google Slides, Google Calendar, Google Meets, GMAIL, Google Analytics, Google Ads, YouTube, Business Profile Manager, Forms</p>
+                </CCardBody>
+                <CCardBody>
+                <h6>Microsoft Office:</h6>
+                <p>Word, Excel, PowerPoint, Teams, Outlook, OneNote, Publisher</p>
+                </CCardBody>
+                <CCardBody>
+                <h6>CRM:</h6>
+                <p>SalesForce, HubSpot, ConvertKit, FreshSales, RQ4</p>
+                </CCardBody>
+                <CCardBody>
+                  <h6>Adobe:</h6>
+                  <p>Illustrator, Photoshop, PDF Editor</p>
+                </CCardBody>
+                <CCardBody>
+                  <h6>Digital Illustrators, Creation, and Website Management:</h6>
+                  <p>Canva, ClipStudio Art, Krita, WordPress, Wix</p>
+                </CCardBody>
+                <CCardBody>
+                <h6>Digital Proficiencies:</h6>
+                <p>SEO, SEM, SMM, SMO, Email Marketing, Content Marketing, Digital Marketing, Newsletter Management</p>
+                </CCardBody>
+        </CCard>
+        <br />
+        <br />
+        <CCard>
+            <CCardBody>
+              <h4>Customer Service, Sales, and Finance:</h4> 
+            </CCardBody>
+              <CCardBody>
+              <h5>Customer Service:</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Customer Retention:</h6>
+                <p>Client engagement, Relationship building, Customer satisfaction, Tailored solutions</p>
+                </CCardBody>
+                <br />
+              <CCardBody>
+                <h5>Sales:</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Upselling:</h6>
+                <p>Product knowledge, Sales techniques, Customer needs, Product demonstrations</p>
+                </CCardBody>
+                <CCardBody>
+                <h6>Lead Generation:</h6>
+                <p>Client engagement, Relationship building, Customer satisfaction, Tailored solutions</p>
+                </CCardBody>
+                <br />
+              <CCardBody>
+                <h5>Finance:</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Financial Analysis:</h6>
+                <p>Financial statements, Budgeting, Forecasting, Financial modeling, Financial reporting</p>
+                </CCardBody>
+                <CCardBody>
+                <h6>Financial Management:</h6>
+                <p>Investment management, Cash flow management, Risk management, Financial planning, Financial strategy</p>
+                </CCardBody>
+        </CCard>
+        <br />
+        <br />
+        <CCard>
+            <CCardBody>
+                <h4>Organizational Skills:</h4>
+            </CCardBody>
+              <CCardBody>
+              <h5>Project Management:</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Project Management:</h6>
+                <p>Project planning, Project execution, Project monitoring, Project closing, Project documentation</p>
+                </CCardBody>
+                <br />
+              <CCardBody>
+                <h5>Time Management:</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Time Management:</h6>
+                <p>Task prioritization, Task delegation, Task tracking, Task completion</p>
+                </CCardBody>
+        </CCard>
+        <br />
+        <br />
+        <CCard>
+            <CCardBody>
+                <h4>Languages:</h4>
+            </CCardBody>
+              <CCardBody>
+              <h5>English:</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Fluency Level:</h6>
+                <p>Native/Mother Tongue</p>
+                </CCardBody>
+                <br />
+              <CCardBody>
+                <h5>Main Filipino Dialect: Tagalog</h5>
+              </CCardBody>
+                <CCardBody>
+                <h6>Fluency Level:</h6>
+                <p>Native/Mother Tongue</p>
+                </CCardBody>
+        </CCard>
+        <br />
+        <br />
+        <CCard>
+            <CCardBody>
+                <h3>Relevant Certifications</h3>
             </CCardBody>
         </CCard>
         <br />
         <br />
         <CCard>
             <CCardBody>
-                This is some text within a card body.
+                <h4>Technical Certifications</h4>
             </CCardBody>
+              <CCardBody>
+                insert technical certifications here
+              </CCardBody>
         </CCard>
         <br />
         <br />
