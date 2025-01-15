873b553dfd78a94179342fa1d2632e19ee6fb177
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-01-15 14:46:43 -0500

    added details to my Resume

--- CyrlCasalme/src/pages/Resume.jsx
+++ CyrlCasalme/src/pages/Resume.jsx
@@ -17,7 +17,7 @@ export default function Resume() {
             </CCardBody>
             <br />
                 <CCardBody>
-                  <h5>Web Development || Full Stack</h5>
+                  <h5>Web Development || Full Stack:</h5>
                 </CCardBody>
                   <CCardBody>
                   <h6>Front-End & Back-End:</h6>
@@ -29,7 +29,7 @@ export default function Resume() {
                     </CCardBody>
               <br />
               <CCardBody>
-                <h5>Technical Support</h5>
+                <h5>Technical Support:</h5>
               </CCardBody>
                 <CCardBody>
                 <h6>Technical Support:</h6>
@@ -49,7 +49,7 @@ export default function Resume() {
                 </CCardBody>
                 <CCardBody>
                 <h6>CRM:</h6>
-                <p>SalesForce, HubSpot, ConvertKit, FreshSales, RQ4</p>
+                <p>SalesForce, HubSpot, ConvertKit, FreshSales, RQ4, JetPack, Monday.com, Mailchimp, Zapier</p>
                 </CCardBody>
                 <CCardBody>
                   <h6>Adobe:</h6>
@@ -86,7 +86,7 @@ export default function Resume() {
                 </CCardBody>
                 <CCardBody>
                 <h6>Lead Generation:</h6>
-                <p>Client engagement, Relationship building, Customer satisfaction, Tailored solutions</p>
+                <p>Client engagement, Relationship building, Customer satisfaction, Tailored solutions, Cold Calling, Virtual Meetings, Sales Webinars</p>
                 </CCardBody>
                 <br />
               <CCardBody>
@@ -98,7 +98,7 @@ export default function Resume() {
                 </CCardBody>
                 <CCardBody>
                 <h6>Financial Management:</h6>
-                <p>Investment management, Cash flow management, Risk management, Financial planning, Financial strategy</p>
+                <p>Investment management, Cash flow management, Risk management, Financial planning, Financial strategy, Residential Mortgage</p>
                 </CCardBody>
         </CCard>
         <br />
@@ -132,7 +132,7 @@ export default function Resume() {
               </CCardBody>
                 <CCardBody>
                 <h6>Fluency Level:</h6>
-                <p>Native/Mother Tongue</p>
+                <p>Native/Mother Tongue || Biligual Proficiency: Read, Spoken, Written</p>
                 </CCardBody>
                 <br />
               <CCardBody>
@@ -140,7 +140,7 @@ export default function Resume() {
               </CCardBody>
                 <CCardBody>
                 <h6>Fluency Level:</h6>
-                <p>Native/Mother Tongue</p>
+                <p>Native/Mother Tongue || Biligual Proficiency: Read, Spoken, Written</p>
                 </CCardBody>
         </CCard>
         <br />
@@ -153,20 +153,164 @@ export default function Resume() {
         <br />
         <CCard>
             <CCardBody>
-                <h4>Technical Certifications</h4>
+                <h4><a href="https://www.linkedin.com/in/cyrl-casalme-948835181/details/certifications/">Technical, Business, and Digital Marketing Certifications</a></h4>
             </CCardBody>
               <CCardBody>
-                insert technical certifications here
+                <h5>Web Development:</h5>
               </CCardBody>
+                <CCardBody>
+                  <h6>Full Stack Web Development:</h6>
+                  <a href="https://www.virtualbadge.io/certificate-validator?credential=74febba2-2545-4ef2-bf40-5e9020b46d19" alt="Front End Development Certificate">Mimo: Professional Certificate, Full Stack Development | Identification number: 74febba2-2545-4ef2-bf40-5e9020b46d19</a>
+                </CCardBody>
+                <CCardBody>
+                  <h6>Back-End Development:</h6>
+                  <a href="https://www.virtualbadge.io/certificate-validator?credential=edf5908d-2e41-4a11-8616-f65e03b3c329" alt="Back-End Development">Mimo: Back-End Development | Identification number: edf5908d-2e41-4a11-8616-f65e03b3c329</a>
+                </CCardBody>
+                <br />
+              <CCardBody>
+                <h5>Project Management Institute:</h5>
+              </CCardBody>
+                <CCardBody>
+                  <h6>Essential Lesson for First-Time Managers:</h6>
+                  <a href="https://www.linkedin.com/learning/certificates/a98b38fe9269ebc1f8864ac63a0b4b16cec128b24613f699ba9978416ea9f2fa?trk=share_certificate" alt="Project Management Certificate">Project Management Institute: Project Management | Identification number: AW86L2Uzz2Jq2RDMAr7Dvi7KpZmC</a>
+                </CCardBody>
+                <CCardBody>
+                  <h6>Leadership Skills For the Future:</h6>
+                  <a href="https://www.linkedin.com/learning/certificates/5751c532d495f92e7ac660085d54fbf857477eecb61a1d15d7a612bf93daef94?trk=share_certificate" alt="Leadership Skills Certificate">Project Management Institute: Leadership Skills | Identification number: AS3ne9y-srAC7U7t3BZiLxk4Q_4z</a>
+                </CCardBody>
+                <CCardBody>
+                  <h6>Leadership Tips, Tactics, and Advice:</h6>
+                  <a href="https://www.linkedin.com/learning/certificates/5f499eec60da96d1dc85b9d42b41e6f2d6fe14672a88155109707cf2bb8141cf?trk=share_certificate" alt="Leadership Tips Certificate">Project Management Institute: Leadership Tips | Identification number: Abf5LFPosloXmJQemBaG4J7udkcb</a>
+                </CCardBody>
+                <br />
+              <CCardBody>
+                <h5>Digital Marketing:</h5>
+              </CCardBody>
+                <CCardBody>
+                  <h6>Google:</h6>
+                  <a href="https://coursera.org/share/687870e7dc7140b06e213cc0337d48ef" alt="Foundations of Digital Marketing and E-commerce">Foundations of Digital Marketing and E-commerce | Identification Id: https://coursera.org/verify/2DVWMQUJRSRU</a>
+                </CCardBody>
+                <CCardBody>
+                  <h6>LinkedIn Learning:</h6>
+                  <a href="https://www.linkedin.com/learning/certificates/dc3949696a0ffe7cedc525cf66193c0c6e74d3ff66462219d656c6962db1c9e1?trk=share_certificate" alt="Marketing: Copywriting for Social Media">Marketing: Copywriting for Social Media || Identification Id: AbYBa86T3GqzHMPba86YXBLbKrNd </a>
+                </CCardBody> 
         </CCard>
         <br />
         <br />
         <CCard>
             <CCardBody>
-                This is some text within a card body.
+                <a href="https://www.linkedin.com/in/cyrl-casalme-948835181/details/education/" alt="Education Link to LinkedIn"><h3>Education</h3></a>
             </CCardBody>
         </CCard>
-
+            <br />
+        <CCard>
+              <CCardBody>
+                <h4>University of Toronto School of Continuing Studies Boot Camps, Partnered with EdX</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Full Stack Web Development || October 2024 to April 2025</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Full Stack Web Development:</h6>
+                  <p>Continuing Education - Software Engineering Relevant Skills: HTML, CSS, JavaScript; Terminal Commands, DOM Manipulation, Browser APIs, GitHub Copilot, Team collaboration, Agile development, Project demonstration and storytelling; Node.js, TypeScript, Prompt Engineering, Object-oriented programming, Single page React apps, SQL, MVC paradigm, MERN Stack, Testing, CI/CD Pipelines, Python Fundamentals, OOP with Python</p>
+                  </CCardBody>
+                  <br />
+                <CCardBody>
+                  <h5>Full Stack Web Development || October 2024 to April 2025</h5>
+                </CCardBody>
+                  <br />
+                  <CCardBody>
+                  <h6>REMIC, Real Eastate & Mortgage Institute of Canada:</h6>
+                  <p>Market Overview, Mortgage Concepts, Property Ownership, Regulation and Legislation, Application Analysis & Calculation, Fraud
+                    Ethics, Compliance, and Professional Practice, Mortgage Brokerage, Lender and Broker Relationships, Mortgage Products, and Documentation</p>
+                </CCardBody>
+                  <br />
+              <CCardBody>
+                <h4>Mohawk College</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Paralegal, Associates Degree || 2017 to 2019</h5>
+                </CCardBody>
+                  <CCardBody>
+                    <h6>Paralegal:</h6>
+                    <p>Research & Writing, POA and Highway Traffic Act, Administrative law, Criminal Summary Conviction, Tribunals, Employment Law, Practice Management, Residential and Tenancies Act, Small Claims Court, Ethics, Introduction to Law, Statutory Acts Benefits, Contracts and Torts, and Alternative Dispute Resolution</p>
+                  </CCardBody>
+        </CCard>
+        <br />
+        <br />
+        <CCard>
+            <CCardBody>
+                <h3>Work Experience</h3>
+            </CCardBody>
+        </CCard>
+        <br />
+        <CCard>
+              <CCardBody>
+                <h4>General Assembly || February 2024 to October 2024</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Admissions Specialist</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Responsibilities:</h6>
+                  <p>Managed the student application process, conducted interviews, and provided prospective students with course guidance.
+                    || Engaged with over 100 students daily across channels, consistently meeting engagement targets through robust communication and organizational skills.
+                    || Collaborated with cross-functional teams to drive enrollments, aligning support efforts with student needs and feedback.</p>
+                  </CCardBody>
+                  <br />
+              <CCardBody>
+                <h4>8Twelve Mortgage Corp. || September 2023 to February 2024</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Mortgage Agent Level 1, Business for Self || M23007701</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Responsibilities:</h6>
+                  <p>Built and maintained client relationships through financial advisory and mortgage consultation, utilizing in-depth industry knowledge.
+                    || Conducted market research and managed accounts, developing a reliable support network for client inquiries.
+                    || Enhanced customer retention by providing tailored financial advice and support.</p>
+              </CCardBody>
+                  <br />
+              <CCardBody>
+                <h4>Snowball Industries, Inc.  || July 2021 to June 2023</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Business Development Manager, Contract</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Responsibilities:</h6>
+                  <p>Conducted B2B prospecting, managing a high volume of cold calls and emails.
+                    || Oversaw CRM data management, booking meetings, and tracking client interactions to improve customer satisfaction and streamline internal processes.
+                    || Created and refined support processes, actively contributing to an improved customer experience.</p>
+              </CCardBody>
+                  <br />
+              <CCardBody>
+                  <h4>Royal Bank of Canada, RBC  || March 2020 to July 2021</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Banking Advisor</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Responsibilities:</h6>
+                  <p>Managed accounts, including fraud detection, mortgage/loan maintenance, savings, investments, and call handling.
+                    || Identified sales opportunities, provided digital solutions, and scheduled advisor appointments for tailored client support.
+                    || Mentored new advisors on account management and sales strategies.</p>
+              </CCardBody>
+              <br />
+              <CCardBody>
+                  <h4>Fido, Wireless DNA  || August 2015 to March 2020</h4>
+              </CCardBody>
+                <CCardBody>
+                  <h5>Sales Representative</h5>
+                </CCardBody>
+                  <CCardBody>
+                  <h6>Responsibilities:</h6>
+                  <p>Handled account management, fraud checks, de-escalation, upgrades, billing, and client relations.
+                    || Prepared competitive reports, supervised junior staff, managed cash/shipments, and improved team sales strategies.
+                    ||Mentored new representatives in account handling, sales opportunities, and de-escalation techniques.</p>
+              </CCardBody>
+        </CCard>
+        <br />
       </div>
     );
   }
