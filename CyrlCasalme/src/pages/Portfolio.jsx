fa1769bbae31a34779b5859d1c7a518ef7a7f38a
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-01-15 22:57:41 -0500

    design changes

--- CyrlCasalme/src/pages/Portfolio.jsx
+++ CyrlCasalme/src/pages/Portfolio.jsx
@@ -1,41 +1,12 @@
-import React from 'react';
-import { CCard, CCardBody } from '@coreui/react';
+import React, { useState } from 'react';
+// // import cssPic from '../assets/css.png';
+// // import jsPic from '../assets/js.png';
+// // import htmlPic from '../assets/html.png';
 
 export default function Portfolio() {
     return (
-      <div>
-        <h1>Portfolio</h1>
-        <br />
-        <CCard>
-            <CCardBody>
-                This is some text within a card body.
-            </CCardBody>
-        </CCard>
-        <br />
-        <br />
-        <CCard>
-            <CCardBody>
-                This is some text within a card body.
-            </CCardBody>
-        </CCard>
-        <br />
-        <br />
-        <CCard>
-            <CCardBody>
-                This is some text within a card body.
-            </CCardBody>
-        </CCard>
-        <br />
-        <br />
-        <CCard>
-            <CCardBody>
-                This is some text within a card body.
-            </CCardBody>
-        </CCard>
-        <br />
-        <br />
-        <p className="footer"><em>CCASALME || Copyright 2025 © Cyrl Casalme</em></p>
-      </div>
+        <>
+        </>
     );
-  }
-  
+}
+
