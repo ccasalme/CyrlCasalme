50f41fca0feb752fe5f17d0cc288c68a42032574
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-01-21 23:14:23 -0500

    added to portfolio

--- CyrlCasalme/src/pages/AboutMe.jsx
+++ CyrlCasalme/src/pages/AboutMe.jsx
@@ -14,20 +14,21 @@ export default function AboutMe() {
             </CCardBody>
         </CCard>
         <br />
-        <img className="profile" src={profilePic} alt="Profile Picture" />
-        <br />
-        <CCard className="aboutBox">
-            <CCardBody>
-                <p className="aboutMe">
-                <strong>Hi, I’m Cyrl but you can also call me CyCy</strong>—an experienced professional with a dynamic background in customer service, sales, account management, and technical support. As a passionate creator and writer, I explore various genres and share my insights freely, believing in the power of knowledge to help others.
+        <div className="container3">
+            <img className="profile" src={profilePic} alt="Profile Picture" />
+            <br />
+            <CCard >
+                <CCardBody>
+                    <p><strong>Hi, I’m Cyrl but you can also call me CyCy</strong>—an experienced professional with a dynamic background in customer service, sales, account management, and technical support. As a passionate creator and writer, I explore various genres and share my insights freely, believing in the power of knowledge to help others.
                 Currently pursuing a <em>Full Stack Certification</em> through <strong>University of Toronto, Continuing Education, partnered with Edx,</strong> I am eager to merge my technical expertise with my extensive interpersonal skills to uncover innovative opportunities at the intersection of technology and client success. 
                 My diverse experiences have honed my adaptability, creativity, and ability to turn challenges into growth opportunities.
                 I thrive in environments where I can collaborate with teams, optimize processes, and help businesses achieve their goals. 
                 Whether it’s identifying needs, delivering tailored solutions, or providing hands-on support, I’m driven by the chance to make a significant impact.
                 Outside of professional pursuits, I’m a creative enthusiast who loves exploring artistic hobbies and bringing a fresh perspective to every project. Let’s connect and create something meaningful together!
-                </p>
-            </CCardBody>
-        </CCard>
+                    </p>
+                </CCardBody>
+            </CCard>
+        </div>
         <br />
         <Footer />
         <p className="footer"><em>CCASALME || Copyright 2025 © Cyrl Casalme</em></p>
